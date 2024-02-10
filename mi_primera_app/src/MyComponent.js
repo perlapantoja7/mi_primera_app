0689f9f1a162dd9c8c9cb09043c35152e8bbf034
Tecnologías <pc-16@iMac-de-Tecnologias.local>
2024-02-10 10:44:20 -0600

    primer commit

---
+++ mi_primera_app/src/MyComponent.js
@@ -0,0 +1,14 @@
+export function MyComponent()
+{
+    return <h1>Este es mi componente</h1>
+}
+
+export function Presentacion()
+{
+return <div>
+<h1>Mi nombre es Perla Pantoja</h1>
+<h3>Mi edad es 26 años</h3>
+<p>Trabajo en Lear</p>
+</div>
+
+}
